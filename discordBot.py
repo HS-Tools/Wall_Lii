3d26e61ff8ffb5555f551662b2fc633bac936e2f
JimLiu0 <jim.liu@berkeley.edu>
2021-02-16 23:11:58 -0500

    Attempt to multithread discord and twitch

---
+++ discordBot.py
@@ -0,0 +1,25 @@
+import threading
+import os
+import discord
+from currentDay import getCurrentDay
+
+discordBot = discord.Client()
+
+@discordBot.event
+async def on_ready():
+    print('{} connected to discord'.format(discordBot.user))
+
+@discordBot.event
+async def on_message(message):
+    if message.author == discordBot.user:
+        return
+
+    if message.content == '99!':
+        await message.channel.send('hi')
+
+# discordBot.run(os.environ['DISCORD_TOKEN'])
+
+# # Run a thread for the discord bot
+# discordBotThread = threading.Thread(target=discordBot.run, args=(os.environ['DISCORD_TOKEN']))
+# # discordBotThread.daemon = True
+# discordBotThread.start()
