1f939ea886365ce45380babde4d47cd252c007a7
JimLiu0 <jim.liu@berkeley.edu>
2021-03-10 04:50:50 -0500

    Change handler to return

--- lambda-loader/src/handler.py
+++ lambda-loader/src/handler.py
@@ -1,4 +1,6 @@
 import api #DELETEME: I always mess these imports up.
 
-def handler(event,context):
+def handler(event, context):
     print(api.getLeaderboardSnapshot()) #DELETEME: This should work.
+
+    return 
