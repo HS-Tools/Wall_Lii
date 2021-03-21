8e2f9d78c2a2a9f6ad437e8d2fada6687787d474
JimLiu0 <jim.liu@berkeley.edu>
2021-03-21 00:24:29 -0400

    Add code to add rank to object

--- lambda-loader/src/handler.py
+++ lambda-loader/src/handler.py
@@ -11,7 +11,8 @@ def handler(event, context):
     for region in REGIONS:
         for player in snapshot[region].keys(): # ???
             rating = snapshot[region][player]['rating']
+            rank = snapshot[region][player]['rank']
             database.put_item(region, player.decode('utf-8'), rating)
             player = player.decode('utf-8')
-            database.put_item(region=region,player=player,rating=rating)
+            database.put_item(region=region,player=player,rating=rating,rank=rank)
 
