ac7d5837f19f155aa4f7b1ec3f4a7a597a71e153
JimLiu0 <jim.liu@berkeley.edu>
2021-03-21 00:31:16 -0400

    remove duplicate put

--- lambda-loader/src/handler.py
+++ lambda-loader/src/handler.py
@@ -12,7 +12,6 @@ def handler(event, context):
         for player in snapshot[region].keys(): # ???
             rating = snapshot[region][player]['rating']
             rank = snapshot[region][player]['rank']
-            database.put_item(region, player.decode('utf-8'), rating)
             player = player.decode('utf-8')
             database.put_item(region=region,player=player,rating=rating,rank=rank)
 
