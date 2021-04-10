e3e01cb7d1982c12af41d4bc4f216604493605ae
JimLiu0 <jim.liu@berkeley.edu>
2021-04-10 12:32:18 -0400

    Reorder print statement

--- lambda-loader/src/api.py
+++ lambda-loader/src/api.py
@@ -16,9 +16,9 @@ def getLeaderboardSnapshot():
         accounts = json.loads(r.text)['leaderboard']['rows']
         lastUpdated = json.loads(r.text)['leaderboard']['metadata']['last_updated_time']
         # To get UTC Time
-
-        print(f'{region} fetched at {lastUpdated}')
         lastUpdated = lastUpdated.split(' ')[1].split('.')[0]
+        
+        print(f'{region} fetched at {lastUpdated}')
 
         for account in accounts:
             name = account['accountid'].encode('utf-8').lower() # Why is this converted to bytes?
