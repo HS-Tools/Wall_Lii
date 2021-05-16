d3a196c253b50f446eee33c7998f47b9b9e77b63
Quinn Abrahams Work <quinna@wearstrive.com>
2021-05-16 13:21:22 -0700

    worked on building an offline test framework

--- lambda-loader/src/api.py
+++ lambda-loader/src/api.py
@@ -1,28 +1,34 @@
 import requests
 import json
 
-def getLeaderboardSnapshot(regions = ['US', 'EU', 'AP'], gameMode = 'BG'):
+def getLeaderboardSnapshot(regions = ['US', 'EU', 'AP'], gameMode = 'BG', season = None, verbose = False):
     ratingsDict = {region : {} for region in regions}
+    updatedDict = {region : None for region in regions}
 
     for region in regions:
+        ## not supplying season always gets latest
         apiUrl = f'https://playhearthstone.com/en-us/api/community/leaderboardsData?region={region}&leaderboardId={gameMode}'
+        if season != None: ## used for test code to pull a known season results
+            apiUrl = f'{apiUrl}&seasonId={season}'
         r = requests.get(apiUrl)
 
-        jsonStr = json.loads(r.text)
-        season = jsonStr['seasonId']
-        accounts = jsonStr['leaderboard']['rows']
-        lastUpdated = " ".join(jsonStr['leaderboard']['metadata']['last_updated_time'].split(' ')[0:2])
+        JSON = json.loads(r.text)
+        season = JSON['seasonId']
+        accounts = JSON['leaderboard']['rows']
+        updatedDict[region] = " ".join(JSON['leaderboard']['metadata']['last_updated_time'].split(' ')[0:2])
 
-        print(f'{region} fetched at {lastUpdated}')
+        if verbose: print(f'{region} fetched at {updatedDict[region]}')
 
         for account in accounts:
-            print(f'\t{account}')
+            if verbose: print(f'\t{account}')
             name = account['accountid'].encode('utf-8').lower() # Why is this converted to bytes?
             ratingsDict[region][name] = {'rank': account['rank'], 'rating': account['rating']}
 
-    return (ratingsDict, lastUpdated)
+    return (ratingsDict, updatedDict, season)
 
-if __name__ == '__main__':
-    getLeaderboardSnapshot(['US'])
-    getLeaderboardSnapshot(['AP', 'EU'])
-    getLeaderboardSnapshot()
+if __name__ == '__main__': ## run the function if this program is called
+    ratingsDict, lastUpdated, season = getLeaderboardSnapshot()
+    for region in ['US', 'EU', 'AP']:
+        print(f'{region} fetched at {updatedDict[region]}')
+        for account in ratingsDict[region].keys():
+            print('\t',ratingsDict[region][account])
