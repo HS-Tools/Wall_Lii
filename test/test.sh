a85b9b693662737aed6a558c5ec27ca37d6d5051
Quinn Abrahams Work <quinna@wearstrive.com>
2021-05-19 19:19:54 -0700

    added set -e to bash script so it exits on a failed test

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
+set -e
+
 echo "test blizzard leaderboard api call"
 pipenv run python testApi.py
-if [[ "$?" -ne 0 ]]; then
-    exit "$?"
-fi
 
 echo "test leaderboard bot"
 pipenv run python testLeaderboard.py
-if [[ "$?" -ne 0 ]]; then
-    exit "$?"
-fi
+
