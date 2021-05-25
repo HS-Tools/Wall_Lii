c9f469690c32eb13329667e1f84ba18d1a64396c
JimLiu0 <jim.liu@berkeley.edu>
2021-05-25 02:35:29 -0400

    Add twitch setup

--- src/twitch.py
+++ src/twitch.py
@@ -6,10 +6,13 @@ from leaderboardBot import LeaderBoardBot
 from parseRegion import parseRegion, isRegion
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
-channels = {'liihs': 'lii',}
+leaderboardBot = LeaderBoardBot()
+
+# Initial setup
+channels = leaderboardBot.getChannels()
+leaderboardBot.updateAlias()
 
 twitchBot = commands.Bot(
     irc_token=os.environ['TMI_TOKEN'],
@@ -18,6 +21,30 @@ twitchBot = commands.Bot(
     prefix=os.environ['BOT_PREFIX'],
     initial_channels=channels.keys()
 )
+
+if __name__ == '__main__':
+
+    @aiocron.crontab('0 * * * *') ## Every hour on the hour check and update channels
+    async def updateChannels():
+        all_channels = leaderboardBot.getChannels()
+        new_channels = []
+        for channel in all_channels.keys():
+            if channel not in channels:
+                channels[channel] = all_channels[channels]
+                new_channels.append(channel)
+        await twitchBot.join_channels(new_channels)
+
+    @aiocron.crontab('5 * * * *') ## Every hour on the 5 check and update the alias table
+    def updateAlias():
+        leaderboardbot.updateAlias()
+
+    twitchThread = threading.Thread(target=twitchBot.run)
+    twitchThread.setDaemon(True)
+    twitchThread.start()
+
+    while True:
+        pass
+
 
 def parseArgs(ctx):
     default = channels[ctx.channel.name]
@@ -68,31 +95,3 @@ async def wall_lii(ctx):
 @twitchBot.command(name='help')
 async def help(ctx):
     await ctx.send('HeyGuys I\'m a bot that checks the BG leaderboard to get data about player ranks and daily MMR fluctuations. I reset daily at Midnight CA time. Try using !bgrank [name] and !bgdaily [name] and !yesterday [name].')
-
-
-if __name__ == '__main__':
-    leaderboardBot = LeaderBoardBot()
-
-    @aiocron.crontab('0 * * * *') ## Every hour on the hour check and update channels
-    async def updateChannels():
-        all_channels = leaderboardBot.getChannels()
-        new_channels = []
-        for channel in all_channels.keys():
-            if channel not in channels:
-                channels[channel] = all_channels[channels]
-                new_channels.append(channel)
-        await twitchBot.join_channels(new_channels)
-
-    @aiocron.crontab('5 * * * *') ## Every hour on the 5 check and update the alias table
-    def updateAlias():
-        leaderboardbot.updateAlias()
-
-
-    twitchThread = threading.Thread(target=twitchBot.run)
-    twitchThread.setDaemon(True)
-    twitchThread.start()
-
-    updateChannels()
-
-    while True:
-        pass
