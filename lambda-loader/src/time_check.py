1ce865d5d730199f5c4088cdd383e08ebf90a308
JimLiu0 <jim.liu@berkeley.edu>
2021-05-28 15:11:41 -0400

    comment out more stuff

--- lambda-loader/src/time_check.py
+++ lambda-loader/src/time_check.py
@@ -3,7 +3,7 @@ import time
 from datetime import date, datetime
 
 while True:
-  print('Current Time: ' + datetime.now().strftime('%H:%M:%S'))
+  # print('Current Time: ' + datetime.now().strftime('%H:%M:%S'))
   api_time_with_microseconds_removed = api.getLeaderboardSnapshot(verbose=False)[1]['US'].split('.')[0]
   api_time = datetime.strptime(api_time_with_microseconds_removed, '%Y-%m-%d %H:%M:%S')
   print('API Time: ' + api_time.strftime('%H:%M:%S'))
