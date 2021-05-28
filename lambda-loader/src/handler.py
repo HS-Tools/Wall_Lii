b2b45c46967d120079adc6c368308cad76a01966
JimLiu0 <jim.liu@berkeley.edu>
2021-05-28 14:56:57 -0400

    Fix logic for initing predictions class and use env vars

--- lambda-loader/src/handler.py
+++ lambda-loader/src/handler.py
@@ -1,6 +1,7 @@
 import api
 import data
-import predictions
+from predictions import Predictions
+from dotenv import load_dotenv
 import os
 
 def add_leaderboards_to_db(database, *args):
@@ -13,6 +14,11 @@ def add_leaderboards_to_db(database, *args):
         database.put_items(region, snapshot[region])
 
 def handlePredictions(database, snapshot, name, region):
+    client_id = os.environ['CLIENT_ID']
+    access_token = os.environ['ACCESS_TOKEN']
+
+    predictions = Predictions(client_id, access_token)
+
     # Automatic prediction module
     lii_rating = database.get_item(region, name)['Ratings'][-1]
     
@@ -24,6 +30,8 @@ def handlePredictions(database, snapshot, name, region):
         predictions.run(False)
 
 def handler(event, context):
+    load_dotenv()
+    
     database = data.RankingDatabaseClient()
     add_leaderboards_to_db(database)
 
