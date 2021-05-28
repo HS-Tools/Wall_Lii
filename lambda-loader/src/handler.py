cb85cc56f998fcd8321195996c210436574d88a6
JimLiu0 <jim.liu@berkeley.edu>
2021-05-28 13:53:33 -0400

    Add initial automatic predictions with dog's name to test

--- lambda-loader/src/handler.py
+++ lambda-loader/src/handler.py
@@ -1,14 +1,27 @@
 import api
 import data
+import predictions
 import os
 
 def add_leaderboards_to_db(database, *args):
     tup = api.getLeaderboardSnapshot(*args)
     snapshot = tup[0]
-    lastUpdated = tup[1]
+
+    handlePredictions(database, snapshot, 'dog', 'US')
 
     for region in snapshot.keys():
         database.put_items(region, snapshot[region])
+
+def handlePredictions(database, snapshot, name, region):
+    # Automatic prediction module
+    lii_rating = database.get_item(region, name)['Ratings'][-1]
+    
+    # Rating gain
+    if int(lii_rating) > int(snapshot[region][name]['rating']):
+        predictions.run(True)
+    # Rating loss
+    if int(lii_rating) > int(snapshot[region][name]['rating']):
+        predictions.run(False)
 
 def handler(event, context):
     database = data.RankingDatabaseClient()
