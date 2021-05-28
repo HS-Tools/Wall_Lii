2acd825c985830dd43dca8845c3e0937b92241cb
Quinn Abrahams Work <quinna@wearstrive.com>
2021-05-28 16:35:12 -0700

    added a time check on updates

--- lambda-loader/src/handler.py
+++ lambda-loader/src/handler.py
@@ -8,7 +8,11 @@ def add_leaderboards_to_db(database, *args):
     lastUpdated = tup[1]
 
     for region in snapshot.keys():
-        database.put_items(region, snapshot[region])
+        timeLast = database.get_time(region)
+        timeCurrent = database.pasre_time(lastUpdated[region])
+        if timeCurrent >= timeLast: ## allow equal time for easy testing
+            database.put_time(region, timeCurrent)
+            database.put_items(region, snapshot[region])
 
 def handler(event, context):
     database = data.RankingDatabaseClient()
