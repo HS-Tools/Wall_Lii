8a79dddc846995408a45b9b9631722a89a609d43
JimLiu0 <jim.liu@berkeley.edu>
2022-02-13 23:00:51 -0800

    Add nicholena buddy

--- src/buddies.py
+++ src/buddies.py
@@ -79,5 +79,6 @@ buddyDict = {
     "hapabear": ('HapaBear', 'Bubba is a Tier 2 4/3. Ability: There is a .5 chance of unplugging your PC during a crucial turn, knocks out the power grid in a 4 block radius', 'Golden Bubba is a Tier 2 8/6. Ability: Unplugging your PC during a crucial turn, knocks out the power grid in a 4 block radius'),
     "donnysc": ("Scapegoat is a tier 1 1/1. Ability: Battlecry: Give Donny a reason to blame RNG for a lost game.", "Golden Scapegoat is a tier 1 2/2. Ability: Give Donny multiple reasons to blame RNG for a lost game."),
     "seouljaboy": ('SeouljaBoy', 'SeoulYaBoy is a Tier 7 1/1. Ability: Bops you every time you hit on Lii.',  'Golden SeoulYaBoy is a Tier 7 2/2. Ability: Convinces Lii to start his dating show.'),
-    "sunbaconrelaxer": ('SunBaconRelaxer', "Rice Cooker is a Tier 2 0/3. Ability: Taunt. Deathrattle: Get a random new Bartender")
+    "sunbaconrelaxer": ('SunBaconRelaxer', "Rice Cooker is a Tier 2 0/3. Ability: Taunt. Deathrattle: Get a random new Bartender"),
+    "nicholena": ('Nicholena', 'Tufa is a Tier 3 6/6. Battlecry: Force the entire lobby to concede and stare in awe at this cute floof.')
 }
