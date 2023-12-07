b3d8e8df70fca3f063d878160cf530e9afc81e55
JimLiu0 <jim.liu.95@gmail.com>
2023-12-07 10:35:52 -0500

    update season

--- lambda-loader/src/api.py
+++ lambda-loader/src/api.py
@@ -30,7 +30,7 @@ def parseSnapshot(text, verbose=False, region="Unknown"):
 def getLeaderboardSnapshot(
     regions=["US", "EU", "AP"],
     gameMode="BG",
-    season=10,
+    season=11,
     verbose=False,
     total_count=1000,
 ):
