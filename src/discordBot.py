ffa62044b7a62a665d7896388e5171e6b1a9bca8
JimLiu0 <jim.liu.95@gmail.com>
2024-04-07 08:12:22 -0700

    Update cron

--- src/discordBot.py
+++ src/discordBot.py
@@ -208,7 +208,7 @@ async def addchannel(ctx, channel_name: str, player_name: str):
 #     await ctx.respond(f"{channel} will have wall_lii removed")
 
 
-@aiocron.crontab("59 7 * * *")
+@aiocron.crontab("59 6 * * *")
 async def sendDailyRecap():
     climbers = leaderboardBot.getMostMMRChanged(5, True)
     losers = leaderboardBot.getMostMMRChanged(5, False)
